c6dba3a37b99fa2dd9546307e30bfc644cbab423
DucPhamH <ducphamhong2@gmail.com>
2023-08-15 02:44:30 +0700

    cai expressjs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const names = 'Dư Thanh Đượ'
-console.log(names)
+import express, { Express, Request, Response } from 'express'
+const app: Express = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

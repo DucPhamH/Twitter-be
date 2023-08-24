b279b43da1d14a44587a0bd870023c73050edc5b
DucPhamH <ducphamhong2@gmail.com>
2023-08-24 10:40:50 +0700

    upload video

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
-import { UPLOAD_DIR } from './constants/dir'
+import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import staticRouter from './routes/static.routes'
 config()
 databaseService.connect()
@@ -26,7 +26,7 @@ app.use('/user', usersRouter)
 app.use('/medias', mediasRouter)
 
 app.use('/static', staticRouter)
-// app.use('/static', express.static(UPLOAD_DIR))
+// app.use('/static', express.static(UPLOAD_IMAGE_DIR))
 
 app.use(defaultErrorHandler)
 app.listen(port, () => {

0de31042dc815e3fb38562753ce0c0bd3b98108b
DucPhamH <ducphamhong2@gmail.com>
2023-08-25 09:54:23 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 import staticRouter from './routes/static.routes'
 import cors from 'cors'
 config()
-databaseService.connect()
 const app: Express = express()
-app.use(cors())
 const port = process.env.PORT || 4000
+databaseService.connect()
+
+app.use(cors())
+
 initFolder()
 
 app.use(express.json())
